c4be65d91fc92f59ef1f576dfdb6286f65614d2f
Francisco Gómez <espectalll123@kydara.com>
2016-03-09 07:39:37 +0100

    Trying to do something...

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,34 @@
 extern crate gtk;
 
-use gtk::widgets::Builder;
-use gtk::HeaderBar;
+use gtk::{HeaderBar, Window};
 use gtk::traits::*;
-use gtk::signal::Inhibit;
+use gtk::signal::*;
 
 fn main() {
     if gtk::init().is_err() {
         println!("バカ!!! >_<");
-        return;
     }
     
-    let window = gtk::Window::new(gtk::WindowType::Toplevel).unwrap();
+    let window = gtk::Window::new(gtk::WindowType::Toplevel);
     window.set_default_size(600, 400);
     
-    let header_bar = gtk::HeaderBar::new().unwrap();
+    let header_bar = gtk::HeaderBar::new();
     header_bar.set_title("GTK is awesome");
     header_bar.set_subtitle("The title is telling the truth!");
     header_bar.set_show_close_button(true);
     window.set_titlebar(&header_bar);
     
-    let button_a = gtk::Button::new_from_icon_name("gtk-refresh", 0).unwrap();
-    let button_b = gtk::Button::new_from_icon_name("gtk-copy", 0).unwrap();
+    let button_a = gtk::Button::new_from_icon_name("gtk-refresh", 0);
+    let button_b = gtk::Button::new_from_icon_name("gtk-copy", 0);
     button_a.set_always_show_image(true);
     button_b.set_always_show_image(true);
     header_bar.add(&button_a);
     header_bar.add(&button_b);
     
     button_a.connect_clicked(|_| {
-        unsafe {
-            let builder = Builder::new_from_file("headerbars.glade").unwrap();
-            let new_header_bar: HeaderBar = builder.get_object("extraHeader1").unwrap();
-            // How do I get the window here!?
-            // window.set_titlebar(&new_header_bar);
-        }
+        let builder = Builder::new_from_file("headerbars.glade").unwrap();
+        let new_header_bar: HeaderBar = builder.get_object("extraHeader1").unwrap();
+        window.set_titlebar(&new_header_bar);
     });
     button_b.connect_clicked(|_| {
         println!("Heya! ;3");
