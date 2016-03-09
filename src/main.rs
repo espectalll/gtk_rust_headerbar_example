f4c418e5324fd4ce9516d61b3f87c78a7b70bc36
Francisco Gómez <espectalll123@kydara.com>
2016-03-09 12:06:31 +0100

    Things are now better

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 extern crate gtk;
 
-use std::rc::Rc;
-use std::cell::RefCell;
-
 use gtk::{HeaderBar, Builder};
 use gtk::prelude::*;
+
+macro_rules! clone {
+    (@param _) => ( _ );
+    (@param $x:ident) => ( $x );
+    ($($n:ident),+ => move || $body:expr) => (
+        {
+            $( let $n = $n.clone(); )+
+            move || $body
+        }
+    );
+    ($($n:ident),+ => move |$($p:tt),+| $body:expr) => (
+        {
+            $( let $n = $n.clone(); )+
+            move |$(clone!(@param $p),)+| $body
+        }
+    );
+}
 
 fn main() {
     if gtk::init().is_err() {
@@ -12,17 +26,13 @@ fn main() {
     }
     
     let window = gtk::Window::new(gtk::WindowType::Toplevel);
-    let w_handle_o = Rc::new(RefCell::new(window));
-    let w_handle_1 = w_handle_o.clone();
-    let w_handle_2 = w_handle_o.clone();
-    let w_handle_3 = w_handle_o.clone();
-    w_handle_1.borrow_mut().set_default_size(600, 400);
+    window.set_default_size(600, 400);
     
     let header_bar = gtk::HeaderBar::new();
     header_bar.set_title(Some("GTK is awesome"));
     header_bar.set_subtitle(Some("The title is telling the truth!"));
     header_bar.set_show_close_button(true);
-    w_handle_1.borrow_mut().set_titlebar(Some(&header_bar));
+    window.set_titlebar(Some(&header_bar));
     
     let button_a = gtk::Button::new_from_icon_name("gtk-refresh", 0);
     let button_b = gtk::Button::new_from_icon_name("gtk-copy", 0);
@@ -31,22 +41,22 @@ fn main() {
     header_bar.add(&button_a);
     header_bar.add(&button_b);
     
-    button_a.connect_clicked(move |_| {
+    button_a.connect_clicked(clone!(window => move |_| {
         let builder = Builder::new_from_file("headerbars.glade");
         let new_header: HeaderBar = builder.get_object("extraHeader1").unwrap();
-        w_handle_2.borrow_mut().set_titlebar(Some(&new_header));
-    });
-    button_b.connect_clicked(move |_| {
+        window.set_titlebar(Some(&new_header));
+    }));
+    button_b.connect_clicked(clone!(window => move |_| {
         let builder = Builder::new_from_file("headerbars.glade");
         let new_header: HeaderBar = builder.get_object("extraHeader2").unwrap();
-        w_handle_3.borrow_mut().set_titlebar(Some(&new_header));
-    });
+        window.set_titlebar(Some(&new_header));
+    }));
     
-    w_handle_1.borrow_mut().connect_delete_event(|_, _| {
+    window.connect_delete_event(|_, _| {
         gtk::main_quit();
         Inhibit(false)
     });
     
-    w_handle_1.borrow_mut().show_all();
+    window.show_all();
     gtk::main();
 }
